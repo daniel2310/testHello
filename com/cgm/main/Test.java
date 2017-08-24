8d9be1a9740eac0c134225398fb86ef4164ba587
daniel2310 <31306894+daniel2310@users.noreply.github.com>
2017-08-24 16:29:31 +0300

    Update Test.java
    vezi ca am modificat ceva

--- com/cgm/main/Test.java
+++ com/cgm/main/Test.java
@@ -3,7 +3,7 @@ package com.cgm.main;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("This is the master");
+		System.out.println("This is the master by Daniel");
 
 	}
 
